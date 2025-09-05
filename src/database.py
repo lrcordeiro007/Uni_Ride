49e80d29ffdcdbb6a5bca9271076907a3ffcc75c
Luis Renato Pinto Cordeiro <120677147+lrcordeiro007@users.noreply.github.com>
2025-09-05 17:37:46 -0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://root:Destemido_007@localhost:3306/Carona_DB'
 
-engine = create_engine(URL_DATABASE)
+URL_DATABASE = 'postgresql+psycopg2://postgres:12345678@localhost:5432/Carona_DB'
+
+
+engine = create_engine(URL_DATABASE, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
